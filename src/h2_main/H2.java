1e0ee8b1f75ee9dd0f60261f51e4929fad2fcefb
SpaceCrafter29 <stefan.i.dobrea@gmail.com>
2025-11-24 19:35:57 +0100

    H2 bearbeitet

--- src/h2_main/H2.java
+++ src/h2_main/H2.java
@@ -2,6 +2,22 @@ package h2_main;
 
 public class H2 {
     public static void main(String[] args) {
+        int[] a = {1,2,3,4,5,6,7,8,9,10};
+        int[] b = {10,9,8,7,6,5,4,3,2,1};
+        int[] c = {1,2,3,4,5,6,7,8,9};
+        System.out.println(compareArrays(a,c));
 
+    }
+    public static boolean compareArrays(int[] arr1, int[] arr2) {
+        if (arr1.length !=  arr2.length) {
+            return false;
+
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
