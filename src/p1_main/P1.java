91ee22d2dfd82c32b9cf5e16e9afffb874c0b011
SpaceCrafter29 <stefan.i.dobrea@gmail.com>
2025-11-25 16:15:55 +0100

    P2 bearbeitet

--- src/p1_main/P1.java
+++ src/p1_main/P1.java
@@ -2,6 +2,11 @@ package p1_main;
 
 public class P1 {
     public static void main(String[] args) {
-
+        int n = 4;
+        System.out.println(squared(n));
+    }
+    public static int squared(int n) {
+        n++;
+        return n*n;
     }
 }
